def824ff0f9e86a3fef55738debfbe51a6d0b4ac
Ceendormi45 <80924643+Ceendormi45@users.noreply.github.com>
2022-07-29 16:30:23 -0400

    emisor1 y receptor 2 s
    OJO:Aun no tienen mensaje propio

---
+++ EMISOR_1_RECEPTOR_2.ino
@@ -0,0 +1,39 @@
+
+#include <IRremote.h>      //Librería para IR
+
+IRsend irsend;            //Se transmitirán paquetes remotos IR por medio de la variable irsend,
+int RECV = 10;
+IRrecv irrecv(10);        //Se reciben paquetes remotos IR por medio del objeto irrecv
+decode_results results;   // decoficación de los paquetes enviados por ky-005
+
+/*Emisor 1*/
+
+void setup()
+{
+  Serial.begin(9600);                   //Iniciando puerto serial en 9600 bauds
+  irrecv.enableIRIn();                    // Habilita la recepción de datos
+}
+
+void loop() {
+
+ emisor1();
+receptor2();
+}
+void emisor1(){
+
+  irsend.sendNEC(0x00000001, 32);     //se enviara un "A" un mensaje de 8bits
+ // Serial.println("send");            //se imprimirá "send" como confirmación
+  delay(1000);                 //esperaremos 100ms
+  
+  }
+
+/*Receptor 1*/
+/*el que recibe la "A"*/
+
+void receptor2(){
+  if ((irrecv.decode(&results))==0x00000001) {        // Cuando se recibe una U
+    Serial.println(results.value, HEX);   // impresión de los valores recibidos en formato hexadecimal
+    irrecv.resume();                      // Después de recibir, se debe llamar a este para restablecer el receptor y prepararlo para recibir otro código
+  }
+  delay (100);                            // espera para el siguiente valor
+}
